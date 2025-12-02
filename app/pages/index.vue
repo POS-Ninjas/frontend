fca9cb1f706e99ef4c6f08c28692e7b1cb831de2
DroneBreaker <brownierz01@gmail.com>
2025-12-02 16:47:08 +0000

    Change

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,37 +1,9 @@
 <template>
   <div class="flex items-center justify-center min-h-screen bg-gray-50">
-    <UCard class="w-full max-w-md mx-4">
-      <template #header>
-        <h2 class="text-xl font-semibold text-center">Login</h2>
-      </template>
-
-      <UForm @submit="handleLogin" :state="form" class="items-center">
-        <UFormGroup label="TIN" class="mx-4">
-          <UInput v-model="form.tin" type="text" placeholder="Business TIN" class="w-[90%] p-3 px-2" />
-        </UFormGroup>
-
-        <UFormGroup label="Username" class="mx-4">
-          <UInput v-model="form.username" type="text" placeholder="Username" class="w-[90%] p-3 px-2" />
-        </UFormGroup>
-
-        <UFormGroup label="Email" class="mx-4">
-          <UInput v-model="form.email" type="email" placeholder="Email" class="w-[90%] p-3 px-2" />
-        </UFormGroup>
-
-        <UFormGroup label="Password" class="mx-4">
-          <UInput v-model="form.password" type="password" placeholder="Password" class="w-[90%] p-3 px-2" />
-        </UFormGroup>
-
-        <UContainer>
-          <UButton type="submit" color="primary" :loading="loading" class="w-full mt-4 justify-center items-center text-white">Login</UButton>
-        </UContainer>
-      </UForm>
-
-      <p class="mt-4 text-sm text-center text-gray-500">
-        Don’t have an account?
+    <p class="mt-4 text-sm text-center text-gray-500">
+        Hello
         <NuxtLink to="/register" class="text-primary-600">Register</NuxtLink>
       </p>
-    </UCard>
   </div>
 </template>
 
@@ -48,27 +20,4 @@ definePageMeta({ layout: 'auth' })
 */
   // const { login } = useAuth()
 
-  const form = reactive({
-    business_tin: '',
-    username: '',
-    email: '',
-    password: ''
-  })
-  const loading = ref(false)
-
-/**
-  * CLICK HANDLERS
-*/
-  const handleLogin = async (e) => {
-    e.preventDefault()
-    loading.value = true
-    try {
-        await login(form.business_tin, form.username, form.email, form.password)
-        navigateTo('/dashboard')
-    } catch (err) {
-        alert(err)
-    } finally {
-        loading.value = false
-    }
-  }
 </script>
